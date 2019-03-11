5e3d525b52fd0608b89a304375a66ce72a1d92e3
victoriagonzalez58 <Victoria.Gonzalez-Calderon@gompersstudent.org>
2019-03-11 14:27:28 -0700

    Update Level1CodingExam.java

--- thing110/Level1CodingExam.java
+++ thing110/Level1CodingExam.java
@@ -1,4 +1,41 @@
 
 public class Level1CodingExam {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+public class Level1CodingExam {
+public static void main(String[] args){
+	JFrame frame = new JFrame();
+	frame.setVisible(true);
+	frame.setSize(800, 300);
+	frame.setTitle("exam");
+	JPanel pan = new JPanel();
+	pan.setVisible(true);
+	pan.setName("COLOR TEACHER");
+	frame.add(pan);
+	frame.pack();
+	JButton red = new JButton();
+	red.addMouseListener(null);
+	red.setBackground(red);
+	red.setOpaque(true);
+	JButton blue = new JButton();
+	blue.addMouseListener(null);
+	
+	JButton green = new JButton();
+	green.addMouseListener(null);
+	JButton yellow = new JButton();
+	yellow.addMouseListener(null);
+	public void JButton (String name){
+		this.name();
+		setBackground(Color backgroundColor)
+		this.setOpaque(boolean isOpaque);
+		}
+	}
+
+
+	
+}
+
+}
 }
